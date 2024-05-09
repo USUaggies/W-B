85c1fd6f3d4f83c46a9e3e7663fe2af9c17c3b23
crozmanjb <90274529+crozmanjb@users.noreply.github.com>
2024-05-09 14:58:07 -0600

    remove '.php' from servers

--- scripts/performance.js
+++ scripts/performance.js
@@ -84,7 +84,7 @@ function getWeather() {
             }
         }
     }
-    request.open("GET", "weather.php?q=" + stationID, true);
+    request.open("GET", "weather?q=" + stationID, true);
     request.send();
 }
 
@@ -465,7 +465,7 @@ function getRunways(weatherData) {
             }
         }
     }
-    request.open("GET", "airports.php?q=" + weatherData['station_id'], true);
+    request.open("GET", "airports?q=" + weatherData['station_id'], true);
     request.send();
 }
 
