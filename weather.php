0849196e69fd51b710282c550ed63db349a4a140
Crozman Bentley <crozmanjb@gmail.com>
2025-09-05 18:16:44 -0600

    fixed issue with new weather api

--- weather.php
+++ weather.php
@@ -7,14 +7,14 @@ if (!array_key_exists('q', $_GET)) {
 $identifier = strtolower($_GET['q']);
 $metarRaw = simplexml_load_file('https://aviationweather.gov/api/data/metar?ids='.$identifier.'&format=xml&taf=false') or die("Error: Cannot get METAR");
 $metar = '{"metar":' . json_encode($metarRaw->data->METAR);
-$tafRaw = simplexml_load_file('https://aviationweather.gov/api/data/taf?ids='.$identifier.'&format=xml&metar=false');
+$tafResults = file_get_contents('https://aviationweather.gov/api/data/taf?ids='.$identifier.'&format=xml&metar=false');
+$tafRaw;
 $taf;
-
-if((string) $tafRaw->data->attributes()->num_results == "0") {
-	$taf = ',"taf":null}';
+if ($tafResults) {
+    $tafRaw = simplexml_load_string($tafResults, 'SimpleXMLElement', LIBXML_NOCDATA);
+    $taf = ',"taf":' . json_encode($tafRaw->data->TAF) . '}';
 } else {
-	$taf = ',"taf":' . json_encode($tafRaw->data->TAF) . '}';
+    $taf = ',"taf":null}';
 }
-
 echo($metar.$taf);
 ?>
