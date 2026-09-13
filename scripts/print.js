1cc6918aae111aca6e245ad7e8ccafb9f0f0d36f
Crozman Bentley <crozmanjb@gmail.com>
2026-09-12 21:10:33 -0600

    Fixed print issue on mobile again

--- scripts/print.js
+++ scripts/print.js
@@ -1,6 +1,9 @@
 const zeroPad = (num, places) => String(num).padStart(places, '0');
-document.addEventListener("load", () => {
-    document.getElementsByClassName("dfwid-tab")[0].parentElement.removeChild(document.getElementsByClassName("dfwid-tab")[0]);
+document.addEventListener("DOMContentLoaded", () => {
+  const el = document.querySelector(".dfwid-tab");
+  if (el) {
+    el.remove();
+  }
 });
 function fillData() {
     /**Main call to fetch all data from local or session storage and call all the fill functions**/
